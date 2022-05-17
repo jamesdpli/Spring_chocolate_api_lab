77b629adb6703a67eb54c9505cc1810a4a7340a0
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-05-17 16:36:36 +0100

    Add SHOW request (By id) to ChocolateController and EstateController class

--- src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
+++ src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
@@ -3,11 +3,15 @@ package com.bnta.chocolate.controllers;
 import com.bnta.chocolate.models.Chocolate;
 import com.bnta.chocolate.repositories.ChocolateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("chocolates")
@@ -20,6 +24,12 @@ public class ChocolateController {
     @GetMapping
     public List<Chocolate> getAllChocolates(){
         return chocolateRepository.findAll();
+    }
+
+//  SHOW
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<Chocolate>> getChocolateById(@PathVariable Long id){
+        return new ResponseEntity<>(chocolateRepository.findById(id), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/bnta/chocolate/controllers/EstateController.java
+++ src/main/java/com/bnta/chocolate/controllers/EstateController.java
@@ -3,11 +3,15 @@ package com.bnta.chocolate.controllers;
 import com.bnta.chocolate.models.Estate;
 import com.bnta.chocolate.repositories.EstateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("estates")
@@ -16,9 +20,16 @@ public class EstateController {
     @Autowired
     EstateRepository estateRepository;
 
+//  GET
     @GetMapping
     public List<Estate> getAllEstates(){
         return estateRepository.findAll();
+    }
+
+//  SHOW
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<Estate>> getEstateById(@PathVariable Long id){
+        return new ResponseEntity<>(estateRepository.findById(id), HttpStatus.OK);
     }
 
 }
